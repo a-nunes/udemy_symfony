c6fbc77998d56d3bab19ca2c25035f9e24268875
Artur Nunes <art.nunes@gmail.com>
2019-12-08 15:25:59 -0300

    Going on with Udemy Symfony Course. Here I've just learned how Routes, Controllers, Entities and Migrations works.

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default", name="default")
+     */
+    public function index()
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => $users,
+        ]);
+    }
+}
