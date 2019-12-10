8c172b20048c9e56fb0d707563d9eb7caf9842cf
Artur Nunes <art.nunes@gmail.com>
2019-12-10 15:14:12 -0300

    Ending the Controller part.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,15 +5,30 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Services\GiftsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="default")
      */
-    public function index(GiftsService $gifts)
+    public function index(GiftsService $gifts, Request $request)
     {
+
+        $this->addFlash(
+            'notice',
+            'Your data was properly saved.'
+        );
+
+        $this->addFlash(
+            'warning',
+            'You\'ve lost your data, please try again.'
+        );
+
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
